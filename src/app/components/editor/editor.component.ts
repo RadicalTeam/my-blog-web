3f880ac176edf40c21a85d0b5604789baedda89c
Piao Luo <piluo@thoughtworks.com>
2018-08-03 15:40:46 +0800

    add options to hide preview mode and add brief to body to upload

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BlogService} from '../../services/blog.service';
 import {Article} from '../../types/article';
+import {BRIEF_MAX_LENGTH} from '../../constant/article';
 
 @Component({
   selector: 'app-editor',
@@ -12,6 +13,9 @@ export class EditorComponent implements OnInit {
   article: Article = new Article();
   mode = 'editor';
   height = window.screen.availHeight - 20 + 'px';
+  options = {
+    hideIcons: ['TogglePreview'],
+  };
   constructor(
     private blogService: BlogService,
   ) { }
@@ -23,6 +27,7 @@ export class EditorComponent implements OnInit {
     const nodeDOM = document.body.getElementsByClassName('preview-panel');
     const parentNode: HTMLElement = nodeDOM[0].parentElement;
     this.article.content = parentNode.innerHTML;
+    this.article.brief = parentNode.innerText.substring(0, BRIEF_MAX_LENGTH);
     this.blogService.addNewBlog(this.article).subscribe((res) => {
       console.log('succeed');
     });
