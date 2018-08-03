cc0c49778ebeac3521830f33fc408898a46a08c6
Piao Luo <piluo@thoughtworks.com>
2018-08-03 15:39:06 +0800

    add default redirect page to list page

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     path: 'article/:id', component: ContentComponent,
   },
   {
-    path: '**', component: ArticleListComponent,
+    path: '**', redirectTo: '',
   }
 ];
 
