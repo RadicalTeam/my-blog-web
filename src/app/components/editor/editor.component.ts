ef80c703ce31f26e101ae306f7fe3b898efc1cf5
Piao Luo <piluo@thoughtworks.com>
2018-08-06 18:59:17 +0800

    add target for editor area and preview area to optimize their way to get innerText

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {BlogService} from '../../services/blog.service';
 import {Article} from '../../types/article';
 import {BRIEF_MAX_LENGTH} from '../../constant/article';
@@ -10,6 +10,8 @@ import {BRIEF_MAX_LENGTH} from '../../constant/article';
 })
 export class EditorComponent implements OnInit {
 
+  @ViewChild('editor') editor: ElementRef;
+  @ViewChild('preview') preview: ElementRef;
   article: Article = new Article();
   mode = 'editor';
   content = '';
@@ -21,10 +23,8 @@ export class EditorComponent implements OnInit {
   }
 
   save() {
-    const previewDOM = document.body.querySelector('#preview');
-    const editorDom = document.body.querySelector('#editor');
-    this.article.brief = previewDOM.innerText.substring(0, BRIEF_MAX_LENGTH);
-    this.article.mdSource = editorDom.innerText;
+    this.article.brief = this.preview.nativeElement.innerText.substring(0, BRIEF_MAX_LENGTH);
+    this.article.mdSource = this.editor.nativeElement.innerText;
 
     this.blogService.addNewBlog(this.article).subscribe((res) => {
       console.log('succeed');
@@ -40,10 +40,8 @@ export class EditorComponent implements OnInit {
   }
 
   publishBlog() {
-    const previewDOM = document.body.querySelector('#preview');
-    const editorDom = document.body.querySelector('#editor');
-    this.article.brief = previewDOM.innerText.substring(0, BRIEF_MAX_LENGTH);
-    this.article.mdSource = editorDom.innerText;
+    this.article.brief = this.preview.nativeElement.innerText.substring(0, BRIEF_MAX_LENGTH);
+    this.article.mdSource = this.editor.nativeElement.innerText;
     this.blogService.publishBlog(this.article).subscribe((res) => {
       console.log('succeed');
     });
